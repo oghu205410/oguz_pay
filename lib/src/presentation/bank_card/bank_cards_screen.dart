04d635b0fed6b75c60571c93abcc05dcbb153b23
Dev1 <dev1@aksahypa.com>
2024-11-20 12:31:45 +0500

    add bank card screen

--- lib/src/presentation/bank_card/bank_cards_screen.dart
+++ lib/src/presentation/bank_card/bank_cards_screen.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../generated/strings.g.dart';
+import '../../common/constant/app_constants.dart';
+import '../../common/utils/extension/extensions.dart';
+import '../../common/widget/space.dart';
 
 @RoutePage()
 class BankCardsScreen extends StatelessWidget {
@@ -14,6 +18,41 @@ class BankCardsScreen extends StatelessWidget {
         title: Text(
           context.t.dashboard.bankCards,
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              CupertinoIcons.plus,
+            ),
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Space.v10,
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return ListTile(
+            tileColor: context.theme.cardColor,
+            title: Text(
+              '**** **** **** 9142',
+              style: context.textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              'Amanov Aman \t',
+              style: context.textTheme.titleSmall,
+            ),
+            trailing: Container(
+              width: 80,
+              height: 40,
+              decoration: BoxDecoration(
+                color: context.colorScheme.primary,
+                borderRadius: BorderRadius.circular(AppConstants.borderRadius / 2),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
