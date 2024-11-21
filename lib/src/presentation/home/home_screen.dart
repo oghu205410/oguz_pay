d31c85c848d34c2c8be1895d8cbb29fc6b1c9a3f
Dev1 <dev1@aksahypa.com>
2024-11-21 12:38:53 +0500

    add loading gif

--- lib/src/presentation/home/home_screen.dart
+++ lib/src/presentation/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../../generated/assets/assets.gen.dart';
 import '../../../generated/strings.g.dart';
 import '../../common/config/router/app_router.gr.dart';
+import '../../common/utils/extension/extensions.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -16,13 +18,27 @@ class HomeScreen extends StatelessWidget {
           context.t.dashboard.home,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pushRoute(
-          PaymentQrScannerRoute(),
-        ),
-        child: Icon(
-          Icons.qr_code,
-        ),
+      body: ListView(
+        children: [
+          Assets.images.loading.image(
+            height: context.height * 0.24,
+          ),
+        ],
+      ),
+      floatingActionButton: _ScanQrButton(),
+    );
+  }
+}
+
+class _ScanQrButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => context.pushRoute(
+        PaymentQrScannerRoute(),
+      ),
+      child: Icon(
+        Icons.qr_code,
       ),
     );
   }
